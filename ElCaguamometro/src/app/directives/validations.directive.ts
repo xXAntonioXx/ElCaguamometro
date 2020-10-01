d621a1b3f28083e554c0e98755d7f3a6aa49e38d
xXAntonioXx <jose_antony11@hotmail.com>
2020-10-01 00:26:52 -0700

    validación del input terminada

--- ElCaguamometro/src/app/directives/validations.directive.ts
+++ ElCaguamometro/src/app/directives/validations.directive.ts
@@ -5,11 +5,27 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class ValidationsDirective {
 
+  inputValuePreType: string;
+
+  // RegEx sets:
+  numberCharRegEx: RegExp = /\d|\.|Backspace/; // typed key regex
+  fullInputRegEx: RegExp = /^(\d*\.?\d*)$/; // full input value regex
+
   constructor(private el: ElementRef) {}
 
-  @HostListener('input', ['$event']) onInputChange(event) {
-    event.stopPropagation();
-    console.log(this.el.nativeElement.value);
+  @HostListener('keydown', ['$event']) onKeyDown(event) {
+    this.inputValuePreType = this.el.nativeElement.value;
+    const keyTyped = event.key;
+    if ( !(this.numberCharRegEx.test(keyTyped)) ) {
+      event.preventDefault();
+    }
+  }
+
+  @HostListener('keyup', ['$event']) onKeyUp(event){
+    const fullValueInput = this.el.nativeElement.value;
+    if ( !(this.fullInputRegEx.test(fullValueInput)) ) {
+      this.el.nativeElement.value = this.inputValuePreType;
+    }
   }
 
 }
